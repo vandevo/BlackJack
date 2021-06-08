40d8efd3235f302a251613ddc14364706b5682e4
Van Vo <54080467+vandevo@users.noreply.github.com>
2021-06-08 16:41:24 +0100

    master

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ let isAlive = true
 let message = ""
 
 let messageEl = document.getElementById("message-el")
+// let sumEl = document.getElementById("sum-el") //
+
+let sumEl = document.querySelector("#sum-el")
 
 function startGame(){
+
+    sumEl.textContent = "Sum: " + sum
 if (sum <= 20) {
     message = "Do you want to draw a new card? 🙂"
 } else if (sum === 21) {
